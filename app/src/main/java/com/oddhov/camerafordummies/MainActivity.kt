88a9fbfb42ed104a3866509262cd29f17a569e89
Sammy Odenhoven <sammy@Sammys-MacBook-Pro-3.local>
2017-11-09 10:42:40 +0100

    First commit

---
+++ app/src/main/java/com/oddhov/camerafordummies/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.oddhov.camerafordummies
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by sammy on 08/11/2017.
+ */
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
